91c8b0c015c404e108a7acded59235e516c77e24
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-19 00:06:40 -0700

    Update net-gargoyle.py

--- net-gargoyle.py
+++ net-gargoyle.py
@@ -3,43 +3,61 @@ import subprocess
 import sqlite3
 
 from subprocess import *
+from datetime import datetime
+
+def timeslice():
+  global timestamp
+  timestamp = datetime.now()
+  return(timestamp)
 
 def interact():
   global conn
   global c
   conn = sqlite3.connect('gargoyle.db')
   c = conn.cursor()
-  print("The DB connection is now open.")
-  
+  timeslice()
+  print(timestamp, " net-gargoyle2: The DB connection is now open.")
+
 def createtable():
+  interact()
   c.execute('''CREATE TABLE nethash
             (date text, nhash text, phash text, nstate)''')
-  c.execute("INSERT INTO nethash VALUES (timestamp, nhash, phash, nstate)")
   conn.commit()
   conn.close()
-  
+  timeslice()
+  print (timestamp, " net-gargoyle2: The DB connection is now closed.")
+
 def printdb():
+  timeslice()
+  print(timestamp, " net-gargoyle2: Contents of nethash table printing...")
   for row in c.execute('SELECT * FROM nethash'):
     print(row)
-     
+  conn.commit()
+  conn.close()
+  timeslice()
+  print(timestamp, " net-gargoyle2: The DB connection is now closed.")
+
 def insertstat():
   try:
-    interact
     sqlite_insert_with_param = """INSERT INTO nethash
                       (date, nhash, phash, nstate)
                       VALUES (?, ?, ?, ?);"""
-     
-    data_tuple = (date, nhash, phash, nstate)
+    timeslice()
+    #data_tuple = (timestamp, nhash, phash, nstate)
+    data_tuple = (timestamp, 'nhash', 'phash', 'nstate')
     c.execute(sqlite_insert_with_param, data_tuple)
     conn.commit()
     conn.close()
-    
+
   except sqlite.Error as error:
-    print("Failed to insert into gargoyle.db sqlite table nethash with:", error)
-    
+    timeslice()
+    print(timestamp, " net-gargoyle2: Failed to insert into gargoyle.db sqlite t                                                                                                                                                             able nethash with:", error)
+
   finally:
     if (conn):
       conn.close()
-      print("The DB connection is now closed.")
+      timeslice()
+      print(timestamp, " net-gargoyle2: The DB connection is now closed.")
+
       
 #### WIP
