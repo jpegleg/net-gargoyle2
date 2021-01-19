9aade85cb7269cb61a95b71c49d78b69a4aeb531
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-18 18:20:34 -0700

    Create net-gargoyle.py
    work in progress, writing out some functions for data structures

---
+++ net-gargoyle.py
@@ -0,0 +1,45 @@
+import re
+import subprocess
+import sqlite3
+
+from subprocess import *
+
+def interact():
+  global conn
+  global c
+  conn = sqlite3.connect('gargoyle.db')
+  c = conn.cursor()
+  print("The DB connection is now open.")
+  
+def createtable():
+  c.execute('''CREATE TABLE nethash
+            (date text, nhash text, phash text, nstate)''')
+  c.execute("INSERT INTO nethash VALUES (timestamp, nhash, phash, nstate)")
+  conn.commit()
+  conn.close()
+  
+def printdb():
+  for row in c.execute('SELECT * FROM nethash'):
+  print(row)
+     
+def insertstat():
+  try:
+    interact
+    sqlite_insert_with_param = """INSERT INTO nethash
+                      (date, nhash, phash, nstate)
+                      VALUES (?, ?, ?, ?);"""
+     
+    data_tuple = (date, nhash, phash, nstate)
+    c.execute(sqlite_insert_with_param, data_tuple)
+    conn.commit()
+    conn.close()
+    
+  except sqlite.Error as error:
+    print("Failed to insert into gargoyle.db sqlite table nethash with:", error)
+    
+  finally:
+    if (conn):
+      conn.close()
+      print("The DB connection is now closed.")
+      
+#### WIP
