0097ed2f830a83f9e493b7a135bc1f9e896ea6ac
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-23 23:36:00 -0700

    Update netMon.py

--- netMon.py
+++ netMon.py
@@ -1,4 +1,4 @@
-'''netMon.py imports netGargoyle.py directly to use the functions and creates a template class and thread'''
+'''Imports netGargoyle.py directly to use the functions and creates a template class and thread.'''
 import threading
 import time
 import sqlite3
@@ -17,7 +17,8 @@ class Gargoyle():
     self.interval = interval # 1 is the default, 0 is heavy, and the value is in seconds
     self.normstate = normstate # 67883f5f0d2945ac53a909bac8fa2ce73029468e07820fd409a5f85ecf2a0924  
                                # normstate  can be any string but is meant to be a hash.
-                               # normstate is a helper tool and does not impact core functionality.
+                               # normstate is a helper tool and does not impact core functionality
+                               # other than that it is required. Use may be in monitor.
 
   def lasthash(self):
     '''Check the current network state hash against the previous network state hash.'''
