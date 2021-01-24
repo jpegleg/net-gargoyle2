67c5e5406573274ac7e66de2c3873f2eab11389f
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-23 23:31:20 -0700

    Update netMon.py

--- netMon.py
+++ netMon.py
@@ -1,3 +1,4 @@
+'''netMon.py imports netGargoyle.py directly to use the functions and creates a template class and thread'''
 import threading
 import time
 import sqlite3
@@ -11,13 +12,15 @@ lock = threading.Lock()
 
 class Gargoyle():
   def __init__(self,name,interval,normstate):
-    '''Gargoyle class has three properties: name, interval, and normstate'''
+    '''Gargoyle class has three properties: name, interval, and normstate.'''
     self.name = name # gargoyle.db
-    self.interval = interval # 1
-    self.normstate = normstate # 67883f5f0d2945ac53a909bac8fa2ce73029468e07820fd409a5f85ecf2a0924
+    self.interval = interval # 1 is the default, 0 is heavy, and the value is in seconds
+    self.normstate = normstate # 67883f5f0d2945ac53a909bac8fa2ce73029468e07820fd409a5f85ecf2a0924  
+                               # normstate  can be any string but is meant to be a hash.
+                               # normstate is a helper tool and does not impact core functionality.
 
   def lasthash(self):
-    '''check the current network state hash against the previous network state hash'''
+    '''Check the current network state hash against the previous network state hash.'''
     while conti > 0:
       ngr.checkdiff()
       print ("Normal state is defined by script argument as >>>",self.normstate)
