526ae5b9bcbf6df74397c4e0ea1806ccf9da7337
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-24 00:46:54 -0700

    linting

---
+++ net_gargoyle.py
@@ -0,0 +1,118 @@
+'''linux honeypot/HIDS module with sqlite for linux'''
+import subprocess
+import sqlite3
+import hashlib
+from datetime import datetime
+import psutil
+
+def procs():
+    '''Use psutil to do a ps process list of all processes then make a hash of it.'''
+    global PSTATE
+    PSTATE = set()
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+        PSTATE.add(proc)
+    PSTATE = str(PSTATE)
+    global PHASH
+    PHASH = hashlib.sha256(PSTATE.encode('utf-8')).hexdigest()
+
+def nets():
+    '''Use linux coreutil to list network EST and LISTEN then make a hash of it.'''
+    global NSTATE
+    NSTATE = set()
+    process = subprocess.Popen(('ss', '-tanu'), stdout=subprocess.PIPE)
+    output = process.communicate()
+    process.wait()
+    NSTATE.add(output)
+    NSTATE = str(NSTATE)
+    global NHASH
+    NHASH = hashlib.sha256(NSTATE.encode('utf-8')).hexdigest()
+
+def lasthash():
+    '''Check the last recorded network hash in the sqlite database.'''
+    timeslice()
+    print(TIMESTAMP, " compare last NHASH and NHASH and updatedb if different.")
+    for row in C.execute('select NHASH from nethash order by "date text" DESC;'):
+        global LHASH
+        LHASH = row
+    CONN.commit()
+    CONN.close()
+    timeslice()
+    print(TIMESTAMP, " net-gargoyle2: The DB CONNection is now closed.")
+
+def timeslice():
+    '''Make a global TIMESTAMP with datetime module.'''
+    global TIMESTAMP
+    TIMESTAMP = datetime.now()
+    return(TIMESTAMP)
+
+def interact():
+    '''Open up the sqlite db CONNection.'''
+    global CONN
+    CONN = sqlite3.connect('gargoyle.db')
+    global C
+    C = CONN.cursor()
+    timeslice()
+    print(TIMESTAMP, " net-gargoyle2: The DB CONNection is now open.")
+
+def createtable():
+    '''Create an empty table for the program.'''
+    interact()
+    C.execute('''CREATE TABLE nethash
+                        (date text, NHASH text, PHASH text, NSTATE, PSTATE)''')
+    CONN.commit()
+    CONN.close()
+    timeslice()
+    print (TIMESTAMP, " net-gargoyle2: The DB CONNection is now closed.")
+
+def printdb():
+    '''Print the entire sqlite db to STDOUT.'''
+    timeslice()
+    print(TIMESTAMP, " net-gargoyle2: Contents of nethash table printing...")
+    for row in C.execute('SELECT * FROM nethash'):
+        print(row)
+    CONN.commit()
+    CONN.close()
+    timeslice()
+    print(TIMESTAMP, " net-gargoyle2: The DB CONNection is now closed.")
+
+def insertstat():
+    '''Insert the current network and ps data into the database.'''
+    try:
+        sqlite_insert_with_param = """INSERT INTO nethash
+                                            (date, NHASH, PHASH, NSTATE, PSTATE)
+                                            VALUES (?, ?, ?, ?, ?);"""
+        timeslice()
+        procs()
+        nets()
+        data_tuple = (TIMESTAMP, NHASH, PHASH, NSTATE, PSTATE)
+        C.execute(sqlite_insert_with_param, data_tuple)
+        CONN.commit()
+        CONN.close()
+
+    except sqlite3.Error as error:
+        timeslice()
+        print(TIMESTAMP, " net-gargoyle2: Failed to insert into gargoyle.db NHASH table:", error)
+
+    finally:
+        if (CONN):
+            CONN.close()
+            timeslice()
+            print(TIMESTAMP, " net-gargoyle2: The DB CONNection is now closed.")
+
+def checkdiff():
+    '''Compare the last hash pulled from the DB to the current network state hash.'''
+    interact()
+    lasthash()
+    nets()
+    nhx = str(LHASH)
+    nhq = (nhx[2:-3])
+    print('my LHASH is', nhq)
+    print('my NHASH is', NHASH)
+    if (nhq == NHASH):
+        timeslice()
+        print(TIMESTAMP, ' net-gargoyle2: Network state is the same <<< No action.')
+    else:
+        timeslice()
+        print(TIMESTAMP, ' net-gargoyle2: Network change detected >>> Updating db...')
+        interact()
+        insertstat()
