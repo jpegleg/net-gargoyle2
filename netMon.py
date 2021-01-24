fc846bafc054bbfb34b4d3ca04988a0068d6d23f
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-01-23 21:00:19 -0700

    Update netMon.py

--- netMon.py
+++ netMon.py
@@ -1,5 +1,7 @@
 import threading
 import time
+import sqlite3
+import sys
 
 import netGargoyle as ngr
 
@@ -7,26 +9,27 @@ shared_resource = False
 
 lock = threading.Lock()
 
+class Gargoyle():
+  def __init__(self,name,interval,normstate):
+    '''Gargoyle class has three properties: name, interval, and normstate'''
+    self.name = name # gargoyle.db
+    self.interval = interval # 1
+    self.normstate = normstate # 67883f5f0d2945ac53a909bac8fa2ce73029468e07820fd409a5f85ecf2a0924
+
+  def lasthash(self):
+    '''check the current network state hash against the previous network state hash'''
+    while conti > 0:
+      ngr.checkdiff()
+      print ("Normal state is defined by script argument as >>>",self.normstate)
+      shared_resource = True
+      time.sleep(myGargoyle.interval)
+
 global conti
 conti = 1
 
-def lasthash():
-  while conti > 0:
-    ngr.checkdiff()
-    shared_resource = True
-    time.sleep(1)
-
-def monitor():
-  while shared_resource == False:
-    time.sleep(1)
-  ngr.timeslice()
-  print(timestamp, f'Thread {threading.currentThread().name} - Detected shared_resource = False')
-  time.sleep(1)
-
-
 if __name__ == '__main__':
-  checker = threading.Thread(target=lasthash, name='Checker')
-  mon = threading.Thread(target=monitor, name='Mon')
-
-  checker.start()
-  mon.start()
+  intv = int(sys.argv[1])
+  global norm
+  norm = str(sys.argv[2])
+  myGargoyle = Gargoyle('gargoyle.db',intv,norm)
+  checker = threading.Thread(target=myGargoyle.lasthash(), name='Checker')
