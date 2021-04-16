efd28c4e44a6e169bb4f32bba7518f7750c8c3d8
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-04-15 23:55:55 -0600

    Update net_set.py

--- net_set.py
+++ net_set.py
@@ -1,5 +1,6 @@
 '''net_set.py imports functions from net_gargoyle.py to create a new gargoyle.db and nethash table then insert current state.'''
 import net_gargoyle as netg
 
+netg.createtable()
 netg.interact()
 netg.insertstat()
