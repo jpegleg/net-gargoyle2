e496fc629eae60cb326fd9267dedfe0eeed2c8d4
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-10-20 12:49:48 -0400

    Pass TXID to netg functions for table operations

--- net_set.py
+++ net_set.py
@@ -1,6 +1,9 @@
 '''net_set.py imports functions from net_gargoyle.py to create a new gargoyle.db and nethash table then insert current state.'''
+import uuid
 import net_gargoyle as netg
+global TXID
+TXID = uuid.uuid4()
 
-netg.createtable()
-netg.interact()
-netg.insertstat()
+netg.createtable(TXID)
+netg.interact(TXID)
+netg.insertstat(TXID)
